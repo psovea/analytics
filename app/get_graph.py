cc256b425fbebce9d98d362e8a11d677e1f10be9
Bram Dekker <bram.dekker@student.uva.nl>
2019-06-18 11:17:47 +0200

    Cleaned up and documented app code #13
    Commented and cleaned up code in the app folder files.

--- app/get_graph.py
+++ app/get_graph.py
@@ -9,7 +9,7 @@ url = "http://18.216.203.6:5000/"
 
 
 def get_stops_json():
-    """Get data about stops from the static database."""
+    """Get data about stops from the static SQL database."""
     query = "get-stops"
     r = requests.get(url + query)
     stops_json = r.json()
@@ -18,7 +18,7 @@ def get_stops_json():
 
 
 def get_line_info_json():
-    """Get data about lines from the static database. """
+    """Get data about lines from the static SQL database."""
     query = "get-line-info?operator=GVB"
     r = requests.get(url + query)
     line_info_json = r.json()
@@ -28,8 +28,13 @@ def get_line_info_json():
 
 def get_coor_weight_json():
     """Make a JSON list with lat, lon and weight (punctuality)."""
+    # Get the current (last 15 sec) weights for the edges from Prometheus.
     current_data = fp.heatmap_punctuality()
+
+    # Make the directed graph.
     G, stops = init_graph(current_data)
+
+    # Make a JSON object that is sent to front-end.
     stop_dict = {}
     for source, dest, attributes in G.edges.data():
         if dest in stop_dict:
@@ -42,13 +47,16 @@ def get_coor_weight_json():
             ]
     return json.dumps(list(stop_dict.values()))
 
+
 def init_graph(weights):
+    """Initialize a directed graph with data from the endpoints."""
     G = nx.DiGraph()
-    G.edges.data('weight', default=1)
+
+    # Get and extract the right info needed to make the graph.
     stops = get_stops_json()
     line_info = get_line_info_json()
 
-    # Fill the graph and plot it.
+    # Make the nodes and edges of the directed graph.
     mg.make_nodes(G, stops)
     mg.make_edges(G, stops, line_info, weights)
     return G, stops
